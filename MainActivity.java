d44f880fb6e07c932037fa998d3548b34a0647d7
dihmuzikien <turbo2547@hotmail.com>
2016-01-26 23:49:02 -0800

    Made MyDialogFragment an inner class

--- MainActivity.java
+++ MainActivity.java
@@ -1,21 +1,26 @@
 package complexability.motionmusicv2;
 
-import android.app.Dialog;
-import android.app.FragmentManager;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    public String testVal = "hello";
+    protected static String testVal = "hello";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -71,5 +76,50 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * DialogFragment class
+     * Created by Sorawis on 1/26/2016.
+     */
+    public static class MyDialogFragment extends DialogFragment implements AdapterView.OnItemClickListener {
+
+
+        String[] listitems = {"item1", "item2", "item3"};
+        ListView myList;
+
+        @Nullable
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View view = inflater.inflate(R.layout.dialog_test_fragment, null, false);
+            myList = (ListView) view.findViewById(R.id.listView);
+            //getDialog().getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+            getDialog().setTitle("Select Effect");
+            return view;
+        }
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            dismiss();
+            testVal = listitems[position];
+            Toast.makeText(getActivity(), listitems[position], Toast.LENGTH_SHORT)
+                    .show();
+
+        }
+
+        @Override
+        public void onActivityCreated(Bundle savedInstanceState) {
+            super.onActivityCreated(savedInstanceState);
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+                    android.R.layout.simple_list_item_1, listitems);
+
+            myList.setAdapter(adapter);
+
+            myList.setOnItemClickListener(this);
+
+        }
+
+
+
     }
 }
